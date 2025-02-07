dc041dda16d7c9d95a7fed48082de076b4771714
changtong <tom_changt@163.com>
2025-02-07 19:07:49 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_example() {
+        // 测试代码
+        assert_eq!(2 + 2, 4);
+    }
 }
